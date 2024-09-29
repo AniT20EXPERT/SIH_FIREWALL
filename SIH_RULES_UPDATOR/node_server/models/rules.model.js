a5b439f56284e99756ecc857021fc4f634fa35f7
AniT20EXPERT <143430197+AniT20EXPERT@users.noreply.github.com>
2024-09-29 15:33:49 +0530

    Update rules.model.js

--- SIH_RULES_UPDATOR/node_server/models/rules.model.js
+++ SIH_RULES_UPDATOR/node_server/models/rules.model.js
@@ -1,18 +1,63 @@
 const mongoose = require('mongoose');
+
+const ips = mongoose.Schema(
+    {
+        ip_address : {
+          ipv4 : {
+            type: String,
+            required: [true]
+          },
+          ipv6 : {
+            type: String,
+            required: [true]
+          }
+        },
+        protocol : {
+          tcp : {
+            type: Boolean
+          },
+          udp : {
+            type: Boolean
+          }
+        },
+        domain_name : {
+          type: String,
+        }
+      }
+)
+const appsSchema = mongoose.Schema(
+    {
+        app_exe_name: {
+            type: String,
+            required: [true, "enter application name!"]
+        },
+        app_exe_path :{
+            type: String,
+            required: [true, "enter application path!"]
+        },
+        app_ips : {
+            type: [ips],
+            required: [true, "set app traffic rules!"]
+        }
+    }
+)
 const RuleSchema = mongoose.Schema(
     {
-        Rule: {
-            type: String,
-            required: [true]
-        },
+        //the following schema item is required to select the rule document in collection
         type: {
             type: String,
-            required: [true, 'Please enter Rule name'],
+            default: 'rule',
+            immutable: true
         },
-        test :{
-            type: String,
-            required: [true]
-        }
+        
+        //schema for the rest:-
+        apps: {
+            type:[appsSchema],
+            required: [true],
+            unique: [true]
+        },
+        
+
     },
     {
         timestamps: true
